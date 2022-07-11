702a7e05550f9e6a7646ee6f3001782779d040d0
Darya Zhukova <Kleotimpatra@mail.ru>
2022-07-12 00:03:44 +0300

    add Randome

--- Example004_div/Program.cs
+++ Example004_div/Program.cs
@@ -1,3 +1,6 @@
-﻿double numA = 12;
-double numB = 5;
+﻿
+int numA = new Random().Next(1,10); //1,2,3...9
+Console.WriteLine(numA);
+int numB = new Random().Next(1,10);
+Console.WriteLine(numB);
 Console.WriteLine(numA / numB);
